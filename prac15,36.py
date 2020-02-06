1b18f4b273cd965140175e3ebf64f63293464de0
sneh2001 <60725758+sneh2001@users.noreply.github.com>
2020-02-06 11:01:36 +0530

    first code

---
+++ prac15,36.py
@@ -0,0 +1,6 @@
+import urllib.request
+webUrl = urllib.request.urlopen('http://wordpress.org/plugins/about/readme.txt')
+print("Result code: "+str(webUrl.getcode()))
+
+data = webUrl.read()
+print(data)
