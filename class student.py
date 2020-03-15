d2ff2f1c3e1a337ccf240eb24cef4f242a9b6613
sneh2001 <60725758+sneh2001@users.noreply.github.com>
2020-03-15 14:50:12 +0530

    Add files via upload

---
+++ class student.py
@@ -0,0 +1,12 @@
+class student:
+    def __init__(self,name,rollno):
+        self.name=name
+        self.rollno=rollno
+    def display(self):
+        print("name:",self.name)
+        print("rollno:",self.rollno)
+name=str(input("please enter your name:"))
+rollno=int(input("please enter your rollno:"))
+s1=student(name,rollno)
+s1.display()
+        
